44091cd52dbacdfba690a4632d616404c7f307cd
H43RO <h2is1234@naver.com>
2020-09-26 21:28:42 +0900

    Collection 자료형

--- Swift_Practice.playground/Pages/2.Collection.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/2.Collection.xcplaygroundpage/Contents.swift
@@ -26,7 +26,7 @@ print(names1)
 print(names1[1 ... 3])
 
 // Dictionary
-//var numberForName: Dictionary<String, Int> = Dictionary<String, Int>()
+// var numberForName: Dictionary<String, Int> = Dictionary<String, Int>()
 var numberForName: [String: Int] = [String: Int]()
 numberForName = [:] // 빈 딕셔너리 생성
 numberForName = ["H43RO": 100, "LULU": 200, "Kong": 300]
@@ -45,3 +45,40 @@ print(numberForName["max"])
 
 print(numberForName.removeValue(forKey: "Kong"))
 print(numberForName.removeValue(forKey: "Kong")) // 없는 값 접근 시 nil 출력
+
+
+// Set
+var integerSet: Set<Int> = Set<Int>()
+integerSet.insert(1)
+integerSet.insert(100)
+integerSet.insert(99)
+integerSet.insert(99)
+integerSet.insert(99)
+integerSet.insert(99)
+
+print(integerSet)
+print(integerSet.contains(1))
+print(integerSet.contains(2))
+
+integerSet.removeFirst()
+print(integerSet)
+
+// Set는 집합 연산에 꽤 유용함
+let setA: Set<Int> = [1, 2, 3, 4, 5]
+let setB: Set<Int> = [3, 4, 5, 6, 7]
+
+// 합집합
+let union: Set<Int> = setA.union(setB)
+print(union) // [2, 4, 5, 6, 7, 3, 1]
+
+// 합집합 오름차순 정렬
+let sortedUnion: [Int] = union.sorted()
+print(sortedUnion) // [1, 2, 3, 4, 5, 6, 7]
+
+// 교집합
+let intersection: Set<Int> = setA.intersection(setB)
+print(intersection) // [5, 3, 4]
+
+// 차집합
+let subtracting: Set<Int> = setA.subtracting(setB)
+print(subtracting) // [2, 1]
