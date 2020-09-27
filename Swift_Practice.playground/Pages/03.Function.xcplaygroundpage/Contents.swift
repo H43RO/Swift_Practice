6389d960a2d40602f07fea46a9fa6e7fc9dd0917
H43RO <h2is1234@naver.com>
2020-09-27 17:52:18 +0900

    함수

--- Swift_Practice.playground/Pages/03.Function.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/03.Function.xcplaygroundpage/Contents.swift
@@ -30,9 +30,11 @@ print(sayHelloToFriends(me:"H43RO", friends: "haha","eric","wing"))
 
 
 // 데이터 타입으로서의 함수
+// 참고로, 전달인자 레이블을 활용하여 호출할 때 to, from 등으로 인자를 전달할 수 있음 (명확하게 표현)
 func greeting(to friend: String, from me: String) {
     print("Hello \(friend)! I'm \(me)")
 }
+greeting(to: "Eric", from: "H43RO") // 전달인자 레이블로만 전달 가능
 
 var someFunction: (String, String) -> Void = greeting(to:from:)
 someFunction("eric", "H43RO")
