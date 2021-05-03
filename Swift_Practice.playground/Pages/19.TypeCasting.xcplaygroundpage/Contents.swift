aee39e136ce450968856c4a38d57ee88c3999265
H43RO <h2is1234@naver.com>
2021-05-03 23:48:05 +0900

    Type Casting

--- Swift_Practice.playground/Pages/19.TypeCasting.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/19.TypeCasting.xcplaygroundpage/Contents.swift
@@ -1,7 +1,111 @@
-//: [Previous](@previous)
 
-import Foundation
+class Person {
+    var name: String = ""
+    func breath() {
+        print("숨을 쉽니다")
+    }
+}
+class Student: Person {
+    var school: String = ""
+    func goToSchool() {
+        print("등교를 합니다")
+    }
+}
+class UniversityStudent: Student {
+    var major: String = ""
+    func goToMT() {
+        print("멤버쉽 트레이닝을 갑니다 신남!")
+    }
+}
+// 인스턴스 생성
+var yagom: Person = Person()
+var hana: Student = Student()
+var jason: UniversityStudent = UniversityStudent()
 
-var greeting = "Hello, playground"
+var result: Bool
+result = yagom is Person // true
+result = yagom is Student // false
+result = yagom is UniversityStudent // false
+result = hana is Person // true
+result = hana is Student // true
+result = hana is UniversityStudent // false
+result = jason is Person // true
+result = jason is Student // true
+result = jason is UniversityStudent // true
 
-//: [Next](@next)
+if yagom is UniversityStudent {
+    print("yagom은 대학생입니다")
+} else if yagom is Student {
+    print("yagom은 학생입니다")
+} else if yagom is Person {
+    print("yagom은 사람입니다")
+} // yagom은 사람입니다
+
+switch jason {
+case is Person:
+    print("jason은 사람입니다")
+case is Student:
+    print("jason은 학생입니다")
+case is UniversityStudent:
+    print("jason은 대학생입니다")
+default:
+    print("jason은 사람도, 학생도, 대학생도 아닙니다")
+} // jason은 사람입니다
+
+switch jason {
+case is UniversityStudent:
+    print("jason은 대학생입니다")
+case is Student:
+    print("jason은 학생입니다")
+case is Person:
+    print("jason은 사람입니다")
+default:
+    print("jason은 사람도, 학생도, 대학생도 아닙니다")
+} // jason은 대학생입니다
+
+// UniversityStudent 인스턴스를 생성하여 Person 행세를 할 수 있도록 업 캐스팅
+var mike: Person = UniversityStudent() as Person
+var jenny: Student = Student()
+//var jina: UniversityStudent = Person() as UniversityStudent // 컴파일 오류
+// UniversityStudent 인스턴스를 생성하여 Any 행세를 할 수 있도록 업 캐스팅
+var jina: Any = Person() // as Any 생략가능
+
+var optionalCasted: Student?
+optionalCasted = mike as? UniversityStudent
+optionalCasted = jenny as? UniversityStudent // nil
+optionalCasted = jina as? UniversityStudent // nil
+optionalCasted = jina as? Student // nil
+
+var forcedCasted: Student
+optionalCasted = mike as! UniversityStudent
+//optionalCasted = jenny as! UniversityStudent // 런타임 오류
+//optionalCasted = jina as! UniversityStudent // 런타임 오류
+//optionalCasted = jina as! Student // 런타임 오류
+
+func doSomethingWithSwitch(someone: Person) {
+    switch someone {
+    case is UniversityStudent:
+        (someone as! UniversityStudent).goToMT()
+    case is Student:
+        (someone as! Student).goToSchool()
+    case is Person:
+        (someone as! Person).breath()
+    }
+}
+doSomethingWithSwitch(someone: mike as Person) // 멤버쉽 트레이닝을 갑니다 신남!
+doSomethingWithSwitch(someone: mike) // 멤버쉽 트레이닝을 갑니다 신남!
+doSomethingWithSwitch(someone: jenny) // 등교를 합니다
+doSomethingWithSwitch(someone: yagom) // 숨을 쉽니다
+func doSomething(someone: Person) {
+    if let universityStudent = someone as? UniversityStudent {
+        universityStudent.goToMT()
+    } else if let student = someone as? Student {
+        student.goToSchool()
+    } else if let person = someone as? Person {
+        person.breath()
+    }
+}
+doSomething(someone: mike as Person) // 멤버쉽 트레이닝을 갑니다 신남!
+doSomething(someone: mike) // 멤버쉽 트레이닝을 갑니다 신남!
+doSomething(someone: jenny) // 등교를 합니다
+doSomething(someone: yagom) // 숨을 쉽니다
